e5b43f9d3d3bf001393ff8204a2139202aeda153
Lennart Kats <lclkats@gmail.com>
2011-04-14 17:09:27 +0000

    Support testing of "extract something" refactorings
    svn path=/spoofax-imp/trunk/org.strategoxt.imp.testing.test/; revision=22536

--- org.strategoxt.imp.testing/editor/java/org/strategoxt/imp/testing/SelectionFetcher.java
+++ org.strategoxt.imp.testing/editor/java/org/strategoxt/imp/testing/SelectionFetcher.java
@@ -14,6 +14,7 @@ import org.spoofax.interpreter.terms.IStrategoTerm;
 import org.spoofax.jsglr.client.imploder.IToken;
 import org.spoofax.terms.TermVisitor;
 import org.strategoxt.imp.runtime.Environment;
+import org.strategoxt.imp.runtime.stratego.StrategoTermPath;
 
 /**
  * Lamely-named class for fetching selections in test
@@ -26,26 +27,41 @@ public class SelectionFetcher {
 	public IStrategoList fetch(IStrategoTerm parsedFragment) {
 		final List<IStrategoTerm> results = new ArrayList<IStrategoTerm>();
 		new TermVisitor() {
+			IStrategoTerm unclosedChild;
 			public void preVisit(IStrategoTerm term) {
 				IToken left = getTokenBefore(getLeftToken(term));
 				IToken right = getTokenAfter(getRightToken(term));
-				if (isOpenQuote(left) && isCloseQuote(right)) {
-					results.add(term);
+				if (isOpenQuote(left)) {
+					if (isCloseQuote(right)) {
+						results.add(term);
+					} else {
+						unclosedChild = term;
+					}
+				}
+			}
+			
+			@Override
+			public void postVisit(IStrategoTerm term) {
+				IToken right = getTokenAfter(getRightToken(term));
+				if (isCloseQuote(right)) {
+					if (unclosedChild != null)
+						results.add(StrategoTermPath.findCommonAncestor(unclosedChild, term));
+					unclosedChild = null;
 				}
 			}
 		}.visit(parsedFragment);
 		return Environment.getTermFactory().makeList(results);
 	}
 
-	private boolean isOpenQuote(IToken left) {
+	protected boolean isOpenQuote(IToken left) {
 		return left != null && left.getKind() == TK_ESCAPE_OPERATOR && isQuoteOpenText(left.toString());
 	}
 
-	private boolean isCloseQuote(IToken right) {
+	protected boolean isCloseQuote(IToken right) {
 		return right != null && right.getKind() == TK_ESCAPE_OPERATOR && !isQuoteOpenText(right.toString());
 	}
 
-	private boolean isQuoteOpenText(String contents) {
+	protected boolean isQuoteOpenText(String contents) {
 		// HACK: inspect string contents to find out if it's an open or close quote
 		if (contents.contains("[")) {
 			return true;
