2f6e686011a52bd049bc12d32c881d079b31f6f8
Daniel A. A. Pelsmaeker <647530+Virtlink@users.noreply.github.com>
2020-06-22 12:04:51 +0200

    Add JavaDoc

--- org.metaborg.spt.core/src/main/java/org/metaborg/spt/core/run/SpoofaxTestExpectationOutput.java
+++ org.metaborg.spt.core/src/main/java/org/metaborg/spt/core/run/SpoofaxTestExpectationOutput.java
@@ -7,12 +7,22 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
-public class SpoofaxTestExpectationOutput implements ISpoofaxTestExpectationOutput {
+/**
+ * Concrete instance of the output of evaluating a test expectation when running the test on Spoofax languages.
+ */
+public final class SpoofaxTestExpectationOutput implements ISpoofaxTestExpectationOutput {
 
     private final boolean success;
     private final List<IMessage> messages;
     private final List<ISpoofaxFragmentResult> fragmentResults;
 
+    /**
+     * Initializes a new instance of the {@link SpoofaxTestExpectationOutput} class.
+     *
+     * @param success whether the test expectation was met
+     * @param messages the messages returned by evaluating the test expectation
+     * @param fragmentResults the results of the fragments that where part of the expectation
+     */
     public SpoofaxTestExpectationOutput(boolean success, Collection<IMessage> messages, Collection<ISpoofaxFragmentResult> fragmentResults) {
         this.success = success;
         this.messages = copyFromCollection(messages);
