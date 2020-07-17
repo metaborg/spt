bb1d39a8cf0c4465d3b118ea8f9b82aa15f56526
Daniel A. A. Pelsmaeker <647530+Virtlink@users.noreply.github.com>
2020-07-17 10:24:03 +0200

    Add Gradle build files

---
+++ settings.gradle.kts
@@ -0,0 +1,13 @@
+rootProject.name = "spt"
+
+pluginManagement {
+  repositories {
+    maven("https://artifacts.metaborg.org/content/groups/public/")
+  }
+}
+
+if(org.gradle.util.VersionNumber.parse(gradle.gradleVersion).major < 6) {
+  enableFeaturePreview("GRADLE_METADATA")
+}
+
+include("org.metaborg.meta.lang.spt")
