2a683ec9c933b1868684018bda9125bb7348a41d
Gabriel Konat <gabrielkonat@gmail.com>
2021-06-10 15:33:36 +0200

    Add Gradle build files and update .gitignore files.

---
+++ org.metaborg.mbt.core/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+  id("org.metaborg.gradle.config.java-library")
+}
+
+fun compositeBuild(name: String) = "$group:$name:$version"
+val spoofax2Version: String by ext
+dependencies {
+  api(platform("org.metaborg:parent:$spoofax2Version"))
+
+  api(compositeBuild("org.metaborg.core"))
+
+  compileOnly("com.google.code.findbugs:jsr305")
+}
