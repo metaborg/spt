e2f712634ab44785d2abf1a990126ebc754f49d4
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-05-27 13:45:42 +0200

    Reformat code to match Kotlin official style

--- org.metaborg.mbt.core/build.gradle.kts
+++ org.metaborg.mbt.core/build.gradle.kts
@@ -1,14 +1,14 @@
 plugins {
-  id("org.metaborg.gradle.config.java-library")
+    id("org.metaborg.gradle.config.java-library")
 }
 
 fun compositeBuild(name: String) = "$group:$name:$version"
 val spoofax2Version: String by ext
 dependencies {
-  api(platform("org.metaborg:parent:$spoofax2Version"))
+    api(platform("org.metaborg:parent:$spoofax2Version"))
 
-  api(compositeBuild("org.metaborg.core"))
+    api(compositeBuild("org.metaborg.core"))
 
-  implementation("jakarta.annotation:jakarta.annotation-api")
-  implementation("jakarta.inject:jakarta.inject-api")
+    implementation("jakarta.annotation:jakarta.annotation-api")
+    implementation("jakarta.inject:jakarta.inject-api")
 }
--- org.metaborg.spt.core/build.gradle.kts
+++ org.metaborg.spt.core/build.gradle.kts
@@ -1,16 +1,16 @@
 plugins {
-  id("org.metaborg.gradle.config.java-library")
+    id("org.metaborg.gradle.config.java-library")
 }
 
 fun compositeBuild(name: String) = "$group:$name:$version"
 val spoofax2Version: String by ext
 dependencies {
-  api(platform("org.metaborg:parent:$spoofax2Version"))
+    api(platform("org.metaborg:parent:$spoofax2Version"))
 
-  api(project(":org.metaborg.mbt.core"))
-  api(compositeBuild("org.metaborg.spoofax.core"))
-  api(compositeBuild("jsglr.shared"))
+    api(project(":org.metaborg.mbt.core"))
+    api(compositeBuild("org.metaborg.spoofax.core"))
+    api(compositeBuild("jsglr.shared"))
 
-  implementation("jakarta.annotation:jakarta.annotation-api")
-  implementation("jakarta.inject:jakarta.inject-api")
+    implementation("jakarta.annotation:jakarta.annotation-api")
+    implementation("jakarta.inject:jakarta.inject-api")
 }
